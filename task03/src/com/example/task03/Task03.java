b1d50917f0970047f362ccd42f241eac5ba538af
dimonodekolon <daluch@mail.ru>
2025-03-14 17:42:50 +0500

    task 3

--- task03/src/com/example/task03/Task03.java
+++ task03/src/com/example/task03/Task03.java
@@ -3,19 +3,19 @@ package com.example.task03;
 public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
+        return centimetre / 100;
+    }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+    public static int getRoundedMetresFromCentimetres(int centimetres) {
+        return Math.round((float) centimetres / 100);
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = getMetreFromCentimetre(345);
-        System.out.println(result);
-         */
+        int result = getMetreFromCentimetre(299);
+        System.out.println("Без округления: " + result);
+
+        int resultRounded = getRoundedMetresFromCentimetres(299);
+        System.out.println("С округлением: " + resultRounded);
     }
 
 }
